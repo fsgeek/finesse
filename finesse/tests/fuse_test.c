ce75c53b810507f4527d9c6046e88ddf69d02743
Tony Mason <fsgeek@cs.ubc.ca>
2020-05-17 18:55:21 +0000

    Add check for passthrough (and get mountpoint!)

--- finesse/tests/fuse_test.c
+++ finesse/tests/fuse_test.c
@@ -20,13 +20,78 @@
 #include <finesse.h>
 #include "finesse_test.h"
 #include "fincomm.h"
+#include <mntent.h>
 
 #if !defined(__notused)
 #define __notused __attribute__((unused))
 #endif // 
 
+static int ServerRunning;
+
+static int finesse_check(void) 
+{
+    int status = 0;
+    finesse_client_handle_t fch;
+
+    if (0 == ServerRunning) {
+        status = FinesseStartClientConnection(&fch);
+        if (0 == status) {
+            sleep(1);
+            status = FinesseStopClientConnection(fch);
+        }
+        if (0 == status) {
+            ServerRunning = 1;
+        }
+    }
+    return status;
+}
+
+static const char *get_mountpoint(const char *name)
+{
+    FILE *mfile;
+    struct mntent *entry = NULL;
+    static char mountpoint[128];
+
+    if (0 != mountpoint[0]) {
+        return mountpoint;
+    }
+
+    mfile = fopen("/etc/mtab", "rt");
+    munit_assert(NULL != mfile);
+
+    entry = getmntent(mfile);
+    while (NULL != entry) {
+        if(NULL != strstr(entry->mnt_fsname,name)) {
+            break;
+        }
+        entry = getmntent(mfile);
+    }
+    if (NULL == entry) {
+        return NULL;
+    }
+
+    assert(strlen(entry->mnt_dir) < sizeof(mountpoint));
+    strncpy(mountpoint, entry->mnt_dir, sizeof(mountpoint));
+
+    return mountpoint;
+}
+
+static MunitResult test_finess_started(
+    const MunitParameter params[] __notused,
+    void *prv __notused)
+{
+    munit_assert(0 == finesse_check());
+    munit_assert(0 != ServerRunning);
+
+    munit_assert(NULL != get_mountpoint("passthrough_ll"));
+
+    return MUNIT_OK;
+}
+
+
 static MunitTest fuse_tests[] = {
     TEST((char *)(uintptr_t)"/null", test_null, NULL),
+    TEST("/started", test_finess_started, NULL),
     TEST(NULL, NULL, NULL),
 };
 
