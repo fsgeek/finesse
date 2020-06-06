9554772be09225bc6cd5a4a13735d7a94e472ffa
Tony Mason <fsgeek@cs.ubc.ca>
2020-06-06 03:45:30 +0000

    Add opendir

--- finesse/bitbucket/opendir.c
+++ finesse/bitbucket/opendir.c
@@ -4,12 +4,55 @@
 // All Rights Reserved
 
 #include "bitbucket.h"
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 void bitbucket_opendir(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
-	(void) req;
-	(void) ino;
-	(void) fi;
+	void *userdata = fuse_req_userdata(req);
+	bitbucket_user_data_t *BBud = (bitbucket_user_data_t *)userdata;
+	bitbucket_inode_t *inode = NULL;
+	int status = 0;
 
-	assert(0); // Not implemented
+	// TODO: should we be doing anything with the flags in fi->flags?
+
+	CHECK_BITBUCKET_USER_DATA_MAGIC(BBud);
+
+	if (FUSE_ROOT_ID == ino) {
+		inode = BBud->RootDirectory;
+	}
+	else {
+		inode = BitbucketLookupInodeInTable(BBud->InodeTable, ino);
+	}
+
+	while (NULL != inode){
+		if (NULL == inode) {
+			status = ENOENT;
+		}
+
+		if (!S_ISDIR(inode->Attributes.st_mode)) {
+			// This inode is not a directory inode
+			status = ENOTDIR;
+		}
+
+		break;
+	}
+
+
+	if (0 != status) {
+		fuse_reply_err(req, status);
+	}
+	else {
+		fi->fh = inode->Attributes.st_mode;
+		fi->cache_readdir = 1; // permit caching
+		fuse_reply_open(req, fi);
+	}
+
+	if (NULL != inode) {
+		BitbucketDereferenceInode(inode);
+		inode = NULL;
+	}
+
 }
