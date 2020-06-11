5415b98c24ccb286bb8bac0c074cbf75b9df358a
Tony Mason <fsgeek@cs.ubc.ca>
2020-06-11 21:58:46 +0000

    Implement unlink.

--- finesse/bitbucket/unlink.c
+++ finesse/bitbucket/unlink.c
@@ -4,12 +4,40 @@
 // All Rights Reserved
 
 #include "bitbucket.h"
+#include <errno.h>
 
 void bitbucket_unlink(fuse_req_t req, fuse_ino_t parent, const char *name)
 {
-	(void) req;
-	(void) parent;
-	(void) name;
+	void *userdata = fuse_req_userdata(req);
+	bitbucket_user_data_t *BBud = (bitbucket_user_data_t *)userdata;
+	bitbucket_inode_t *inode = NULL;
+	int status = EBADF;
 
-	assert(0); // Not implemented
+	CHECK_BITBUCKET_USER_DATA_MAGIC(BBud);
+
+	if (FUSE_ROOT_ID == parent) {
+		inode = BBud->RootDirectory;
+		BitbucketReferenceInode(inode, INODE_LOOKUP_REFERENCE);
+	}
+	else {
+		inode = BitbucketLookupInodeInTable(BBud->InodeTable, parent);
+	}
+
+	while (NULL != inode) {
+		if (BITBUCKET_DIR_TYPE != inode->InodeType) {
+			status = ENOTDIR;
+			break;
+		}
+
+		status = BitbucketDeleteDirectoryEntry(inode, name);
+		break;
+
+
+	}
+
+	fuse_reply_err(req, status);
+
+	if (NULL != inode) {
+		BitbucketDereferenceInode(inode, INODE_LOOKUP_REFERENCE);
+	}
 }
