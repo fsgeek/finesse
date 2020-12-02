fe06b9b0856d255cd08a3cbd9a0b2b53b51be95b
Tony Mason <fsgeek@cs.ubc.ca>
2020-12-02 20:32:54 +0000

    Public definition of getname function; add to build.

---
+++ finesse/include/finesse-utils.h
@@ -0,0 +1,8 @@
+/*
+ * (C) Copyright 2017-2020 Tony Mason
+ * All Rights Reserved
+ */
+
+#include <sys/types.h>
+
+int finesse_get_name_from_fd(int fd, char *buffer, size_t buffer_length);
--- finesse/utils/getname.c
+++ finesse/utils/getname.c
@@ -19,29 +19,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "finesse-utils.h"
+
 // utility function to get a path name for a file descriptor
 // Note: this is going to be system dependent and can fail
 // because the name has changed.
+// boolean return: 0 means failed, anything else means success
 int finesse_get_name_from_fd(int fd, char *buffer, size_t buffer_length)
 {
-    char procbuf[PATH_MAX];
+    char   procbuf[PATH_MAX];
+    size_t len;
 
     if (AT_FDCWD == fd) {
         // just get the current directory
-        return getcwd(buffer, buffer_length) ? NULL : -1 : 0;
+        return getcwd(buffer, buffer_length) ? 1 : 0;
     }
 
-    size_t len;
+    if (buffer_length < PATH_MAX) {
+        // Let the caller fix this
+        errno = EOVERFLOW;
+        return -1;
+    }
 
     snprintf(procbuf, sizeof(procbuf), "/proc/self/fd/%d", fd);
 
     len = readlink(procbuf, buffer, buffer_length);
 
-    if (buffer_length < PATH_MAX) {
-        // Let the caller fix this
-        errno = EINVAL;
-        return -1;
-    }
+    assert(len < buffer_length);  // overflow?
 
-    return fcntl(fd, F_GETPATH, buffer);
+    return 1;
 }
