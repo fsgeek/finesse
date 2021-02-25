3c9f7208bf805fe98bf34ce9b7508ba12fec482d
Tony Mason <fsgeek@cs.ubc.ca>
2021-02-25 17:30:33 +0000

    Add various memcpy tests

--- finesse/tests/perf_test.c
+++ finesse/tests/perf_test.c
@@ -36,11 +36,10 @@
 static const char *TEST_MEMCPY_SIZE_OPTIONS[] = {
     "4096", "65536", "131072", "262144", "524288", "1048576", "2097152", "4194304", NULL,
 };
-// static const char *TEST_MEMCPY_DIRECTION_OPTIONS[] = {"forward", "backward", NULL};
-// static const char *TEST_MEMCPY_ACCESS_OPTIONS[]    = {"sequential", "random", NULL};
+static const char *TEST_MEMCPY_DIRECTION_OPTIONS[] = {"forward", "backward", NULL};
+static const char *TEST_MEMCPY_ACCESS_OPTIONS[]    = {"sequential", "random", NULL};
 static const char *TEST_MEMCPY_ITERATION_OPTIONS[] = {"1000", NULL};
 
-#if 0
 static MunitParameterEnum memcpy_params[] = {
     {.name = (char *)(uintptr_t)TEST_MEMCPY_SIZE, .values = (char **)(uintptr_t)TEST_MEMCPY_SIZE_OPTIONS},
     {.name = (char *)(uintptr_t)TEST_MEMCPY_DIRECTION, .values = (char **)(uintptr_t)TEST_MEMCPY_DIRECTION_OPTIONS},
@@ -48,7 +47,6 @@ static MunitParameterEnum memcpy_params[] = {
     {.name = (char *)(uintptr_t)TEST_MEMCPY_ITERATIONS, .values = (char **)(uintptr_t)TEST_MEMCPY_ITERATION_OPTIONS},
     {.name = NULL, .values = NULL},
 };
-#endif  // 0
 
 static MunitParameterEnum memset_params[] = {
     {.name = (char *)(uintptr_t)TEST_MEMCPY_SIZE, .values = (char **)(uintptr_t)TEST_MEMCPY_SIZE_OPTIONS},
@@ -56,9 +54,27 @@ static MunitParameterEnum memset_params[] = {
     {.name = NULL, .values = NULL},
 };
 
-static unsigned char        tb[4194304];
+static MunitParameterEnum lk_memcpy_params[] = {
+    {.name = (char *)(uintptr_t)TEST_MEMCPY_SIZE, .values = (char **)(uintptr_t)TEST_MEMCPY_SIZE_OPTIONS},
+};
+
+#define TESTBUF_SIZE (1024 * 1024 * 1024)
+static unsigned char        tb[TESTBUF_SIZE];
 static const unsigned char *test_buffer = tb;
-static unsigned char        scratch_buffer[4194304];
+static unsigned char        scratch_buffer[TESTBUF_SIZE];
+
+static size_t fill_testbuffer(void)
+{
+    size_t size = getrandom(tb, sizeof(tb), 0);
+
+    while (size < sizeof(tb)) {
+        size_t s = getrandom(&tb[size], sizeof(tb) - size, 0);
+        munit_assert(0 != s);
+        size += s;
+    }
+
+    return size;
+}
 
 static MunitResult test_memset(const MunitParameter params[], void *arg)
 {
@@ -139,7 +155,6 @@ static MunitResult test_memset_verify(const MunitParameter params[], void *arg)
     return MUNIT_OK;
 }
 
-#if 0
 typedef void *(*copy_test_t)(void *destination, const void *source, size_t size);
 
 static void *test_memcpy_random(void *destination, const void *source, size_t size)
@@ -154,8 +169,84 @@ static void *test_memmove_random(void *destination, const void *source, size_t s
     return memmove(destination, source, size);
 }
 
-static copy_test_t copytests[2][2] = {
-    // axis 1 = access, axis 2 = direction
+// This is the ERMS memcpy of the Linux kernel
+// https://stackoverflow.com/questions/43343231/enhanced-rep-movsb-for-memcpy
+// (I verified this is the current version of Linux as of 2020/02/23)
+//
+static void rep_movsb(void *dst, const void *src, size_t n)
+{
+    __asm__ __volatile__("rep movsb\n\t" : "+D"(dst), "+S"(src), "+c"(n) : : "memory");
+}
+
+static MunitResult test_lk_memcpy(const MunitParameter params[], void *arg)
+{
+    const char *size_str = NULL;
+    size_t      size     = 4096;
+
+    munit_assert(size >= 4096);
+    munit_assert(size <= sizeof(tb));
+
+    (void)arg;
+    size_str = munit_parameters_get(params, TEST_MEMCPY_SIZE);
+
+    if (NULL != size_str) {
+        sscanf(size_str, "%lu", &size);
+    }
+
+    munit_assert(size >= 4096);
+    munit_assert(size <= sizeof(tb));
+
+    fill_testbuffer();
+
+    for (off_t offset = 0; (offset + size) <= sizeof(tb); offset += size) {
+        // So we're going to copy in the specified block sizes
+        rep_movsb((void *)&scratch_buffer[offset], (void *)&test_buffer[offset], size);
+    }
+
+    return MUNIT_OK;
+}
+
+static MunitResult test_sanity_memcpy(const MunitParameter params[], void *arg)
+{
+    size_t         offset     = 0;
+    const unsigned iterations = 1;
+    const size_t   size       = 4096;
+
+    (void)arg;
+    (void)params;
+
+    for (unsigned index = 0; index < iterations; index++) {
+        offset = 0;
+        while ((offset + size) <= sizeof(tb)) {
+            memmove((void *)&scratch_buffer[offset], (void *)&tb[offset], size);
+            offset += size;
+        }
+    }
+
+    return MUNIT_OK;
+}
+
+static MunitResult test_sanity_lk_memcpy(const MunitParameter params[], void *arg)
+{
+    size_t         offset     = 0;
+    const unsigned iterations = 1;
+    const size_t   size       = 4096;
+
+    (void)arg;
+    (void)params;
+
+    for (unsigned index = 0; index < iterations; index++) {
+        offset = 0;
+        while ((offset + size) <= sizeof(tb)) {
+            rep_movsb((void *)&scratch_buffer[offset], (void *)&tb[offset], size);
+            offset += size;
+        }
+    }
+
+    return MUNIT_OK;
+}
+
+static copy_test_t copytests[2][2] = {  // axis 1 = access, axis 2 = direction
     {
         memcpy,              // sequential, forward
         test_memcpy_random,  // random, forward
@@ -228,18 +319,17 @@ static MunitResult test_memcpy(const MunitParameter params[], void *arg)
 
     return MUNIT_OK;
 }
-#endif  //
 
 static MunitResult test_basic(const MunitParameter params[], void *arg)
 {
-    const int    iterations = 100;
-    const size_t size       = 4194304;
+    const int    iterations = 1;
+    const size_t size       = sizeof(tb);
 
     (void)params;
     (void)arg;
 
     for (unsigned index = 0; index < iterations; index++) {
-        munit_assert(sizeof(tb) == getrandom(tb, sizeof(tb), 0));
+        munit_assert(sizeof(tb) == fill_testbuffer());
         munit_assert(scratch_buffer == memcpy(scratch_buffer, test_buffer, size));
     }
 
@@ -248,15 +338,31 @@ static MunitResult test_basic(const MunitParameter params[], void *arg)
 
 static MunitResult test_basic2(const MunitParameter params[], void *arg)
 {
-    const int    iterations = 100;
-    const size_t size       = 4194304;
+    const int    iterations = 1;
+    const size_t size       = sizeof(tb);
 
     (void)params;
     (void)arg;
 
     for (unsigned index = 0; index < iterations; index++) {
-        munit_assert(sizeof(tb) == getrandom(tb, sizeof(tb), 0));
+        munit_assert(sizeof(tb) == fill_testbuffer());
         munit_assert(scratch_buffer == memmove(scratch_buffer, test_buffer, size));
+    }
+
+    return MUNIT_OK;
+}
+
+static MunitResult test_basic3(const MunitParameter params[], void *arg)
+{
+    const int    iterations = 1;
+    const size_t size       = sizeof(tb);
+
+    (void)params;
+    (void)arg;
+
+    for (unsigned index = 0; index < iterations; index++) {
+        munit_assert(sizeof(tb) == fill_testbuffer());
+        rep_movsb(scratch_buffer, test_buffer, size);
     }
 
     return MUNIT_OK;
@@ -266,9 +372,13 @@ static const MunitTest perf_tests[] = {
     TEST("/null", test_null, NULL),
     TEST("/basic", test_basic, NULL),
     TEST("/basic2", test_basic2, NULL),
+    TEST("/basic3", test_basic3, NULL),
     TEST("/memset", test_memset, memset_params),
     TEST("/memset_verify", test_memset_verify, memset_params),
-    //    TEST("/memcpy", test_memcpy, memcpy_params),
+    TEST("/memcpy", test_memcpy, memcpy_params),
+    TEST("/lk_memcpy", test_lk_memcpy, lk_memcpy_params),
+    TEST("/sanity/memcpy", test_sanity_memcpy, NULL),
+    TEST("/sanity/lk_memcpy", test_sanity_lk_memcpy, NULL),
     TEST(NULL, NULL, NULL),
 };
 
@@ -284,7 +394,7 @@ static MunitSuite perftest_suites[10];
 
 MunitSuite *SetupMunitSuites()
 {
-    munit_assert(sizeof(tb) == getrandom(tb, sizeof(tb), 0));
+    munit_assert(sizeof(tb) == fill_testbuffer());
     memset(perftest_suites, 0, sizeof(perftest_suites));
     perftest_suites[0] = perf_suite;
     return perftest_suites;
